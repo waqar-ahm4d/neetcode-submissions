7feba5e53c1ccadf358870a206d695312cc5e562
Waqar Ahmad <thewaqarofficial@gmail.com>
2026-07-08 03:49:08 +0500

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let numMap = new Map();
+        for(let i = 0; i <= nums.length ; i++){
+            
+            let complement = target - nums[i];
+            if(numMap.has(complement)) {
+                return [numMap.get(complement), i];
+            }
+            numMap.set(nums[i], i);
+        }
+        return [];
+    }
+}
