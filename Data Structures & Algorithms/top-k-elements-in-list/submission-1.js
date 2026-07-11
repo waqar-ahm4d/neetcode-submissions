a90a50b9a0b95eb9d06fd42d29897731d3a0d69f
Waqar Ahmad <thewaqarofficial@gmail.com>
2026-07-11 15:33:44 +0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freqMap = new Map();
+        // for(let i=0; i < nums.length; i++) {
+        //     freqMap.set(nums[i], (freqMap.get(nums[i]) || 0)+1);
+        // }
+        for(const num of nums) {
+            freqMap.set(num, (freqMap.get(num) || 0)+1);
+        }
+        const sorted = Array.from(freqMap.keys()).sort(
+            (a,b) => freqMap.get(b) - freqMap.get(a)
+        );
+        return sorted.slice(0,k);
+    }
+}
