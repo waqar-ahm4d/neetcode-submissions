063214a37ccf2bd203c1241e777d6d4fb702470f
Waqar Ahmad <thewaqarofficial@gmail.com>
2026-08-07 07:35:40 +0500

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let left = 0, mid = 0, right = nums.length -1;
+
+        while(mid <= right) {
+            if(nums[mid] === 0) {
+                [nums[left], nums[mid]] = [nums[mid], nums[left]];
+                left++;
+                mid++;
+            } else if (nums[mid] === 1) {
+                mid++;
+            } else {
+                [nums[mid], nums[right]] = [nums[right], nums[mid]];
+                right--;
+            }
+        }
+    }
+    
+}
