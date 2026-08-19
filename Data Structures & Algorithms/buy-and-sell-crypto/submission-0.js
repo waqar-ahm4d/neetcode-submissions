2abde82309363a52caa92c1dc60d9754b04c8ea9
Waqar Ahmad <thewaqarofficial@gmail.com>
2026-08-19 21:28:53 +0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+  /**
+   * @param {number[]} prices
+   * @return {number}
+   */
+//   brute force
+  maxProfit(prices) {
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+      for (let j = i + 1; j < prices.length; j++) {
+        let profit = prices[j] - prices[i];
+        if (profit > maxProfit) maxProfit = profit;
+      }
+    }
+    return maxProfit;
+  }
+}
